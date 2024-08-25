3fc7a1da38cf8d3557a3d4839a826807eb1467fe
Agustín Óscar <reyesglez_oscar@outlook.com>
2024-08-24 20:30:04 -0600

    Initial Commit
    In this commit the project structure is made.

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+    Author: Agustín Reyes
+    
+    Description:
+    This script analyzes TCPview reports through various Cyber ​​threat intelligence platforms.
+"""
+
+# Libraríes and packages.
